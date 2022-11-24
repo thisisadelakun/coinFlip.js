74e7f961ff9f567ff0b0b59153b17889bf4ebce5
Adelakun <adelakunabayomi123@gmail.com>
2022-11-24 22:57:01 +0100

    first commit

---
+++ flip.js
@@ -0,0 +1,46 @@
+let heads = 0;
+let tails =  0;
+let coin = document.querySelector(".main-coin")
+let flipButton = document.querySelector('.flip-button')
+let resetButton = document.querySelector('.reset-coin-button')
+
+flipButton.addEventListener('click', () =>{
+    let i = Math.floor(Math.random() * 2);
+    console.log(i);
+    coin.style.animation = "none";
+    if(i){
+        setTimeout(function(){
+            coin.style.animation = "spin-heads 3s forwards";
+        }, 100);
+        heads++;
+    }
+
+    else{
+        setTimeout(function(){
+            coin.style.animation = "spin-tails 3s forwards";
+        }, 100);  
+        tails++;
+    }
+    setTimeout(updateCounts, 3000);
+    disableButton();
+});
+
+function updateCounts(){
+    document.querySelector(".heads-count").textContent = `Heads: ${heads}`
+    document.querySelector(".tails-count").textContent = `Tails: ${tails}`
+}
+
+function disableButton(){
+    flipButton.disable = true;
+    setTimeout(function(){
+        flipButton.disable = false;
+
+    },3000);
+
+    resetButton.addEventListener("click",() => {
+        coin.style.animation = "none";
+        heads = 0;
+        tails = 0;
+        updateCounts();
+    });
+}
